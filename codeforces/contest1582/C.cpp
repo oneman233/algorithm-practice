28e8c9df6df5e11f0834daa3127558efa5e47ca9
oneman233 <haorandeng@126.com>
2021-11-02 11:12:24 +0800

    codeforces 打着玩

---
+++ codeforces/contest1582/C.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+#define int long long
+#define re(i,a,b) for(int i=a;i<=b;++i)
+#define fo(i,a,b) for(int i=a;i<b;++i)
+#define rre(i,a,b) for(int i=a;i>=b;--i)
+#define all(a) a.begin(),a.end()
+#define F first
+#define S second
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define frt front
+#define bak back
+#define nmsl cout<<"NMSL\n"
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define FAST ios_base::sync_with_stdio(0);cin.tie(0),cout.tie(0);
+#define endl '\n'
+#define P pair
+#define V vector
+#define memmx(a) memset(a,0x3f3f,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+#define memmn(a) memset(a,-0x3f3f,sizeof(a))
+#define lbd lower_bound
+using namespace std;
+typedef double db;
+typedef P<int,int> pii;
+typedef V<int> vi;
+typedef V<pii> vp;
+template<class T> using mxq=priority_queue<T>;
+template<class T> using mnq=priority_queue<T,V<T>,greater<T> >;
+template<typename T> T R(T& x) {
+	char ch=getchar();
+	int s=0,w=1;
+	while(ch<48||ch>57)
+		w=(ch=='-'?-1:1),
+		ch=getchar();
+	while(ch>=48&&ch<=57)
+		s=(s<<1)+(s<<3)+ch-48,
+		ch=getchar();
+	x=s*w;
+	return s*w;
+}
+template<typename T> void W(T x) {
+	if(x<0) putchar('-'),x=-x;
+	if(x>9) W(x/10);
+	putchar(x%10+48);
+}
+void uniq(V<int> &v) {
+	sort(all(v));
+	v.resize(unique(all(v))-v.begin());
+}
+int qpow(int x,int y,int MOD) {
+    x%=MOD;
+    int ret=1;
+    while(y) {
+		if(y&1) ret=ret*x%MOD;
+		x=x*x%MOD;
+		y>>=1;
+	}
+	return ret;
+}
+template <typename T> void debug(const T &t) {cout<<t<<endl;}
+template <typename T,typename...Args>
+void debug(const T &t, const Args&...rest) {cout<<t<<' ';debug(rest...);}
+void debug(vi v) {
+	cout<<"DEBUG"<<endl;
+	for(auto i:v) cout<<i<<' ';
+	cout<<endl;
+}
+template <typename T> void mmin(T& a,T b) {a=min(a,b);}
+template <typename T> void mmax(T& a,T b) {a=max(a,b);}
+template <typename T> P<T,T> mkp(T a,T b) {return make_pair(a,b);}
+template <typename T> P<T,P<T,T> > mkp(T a,T b,T c) {return make_pair(a,make_pair(b,c));}
+
+const int maxn=1e6+5;
+const int inf=0x3f3f3f3f3f3f3f3f;
+const int mod=1e9+7;
+
+//////////////////////////////////////////////////
+
+int _,n;
+string s;
+deque<int> pos;
+int sum[26][maxn];
+
+bool ok() {
+	int l=pos.size();
+	for(int i=0;l-i-1>=0;++i)
+		if(s[pos[i]]!=s[pos[l-i-1]])
+			return 0;
+	return 1;
+}
+
+void pre() {
+	re(i,1,n) {
+		fo(j,0,26)
+			sum[j][i]=sum[j][i-1];
+		sum[s[i]-'a'][i]++;
+	}
+}
+
+int hav(int l,int r,char c) {
+	return sum[c-'a'][r]-sum[c-'a'][l-1];
+}
+
+map<char,int> mp;
+
+signed main() {
+    FAST
+	cin>>_;
+	while(_--) {
+		cin>>n>>s;s='$'+s;
+		pre();
+		mp.clear();
+		re(i,1,n) mp[s[i]]=1;
+		int ans=inf;
+		for(char c='a';c<='z';++c) {
+			if(!mp[c]) continue;
+			pos.clear();
+			re(i,1,n)
+				if(s[i]!=c) pos.pub(i);
+			if(!ok()) continue;
+//			debug(pos);
+			int tmp=n-pos.size();
+			pos.pub(n);
+			pos.puf(1);
+			int l=0,r=pos.size()-1;
+			for(;l<r;l++,r--) {
+				if(l+1!=r) tmp-=min(hav(pos[l],pos[l+1],c),hav(pos[r-1],pos[r],c))*2;
+				else tmp-=hav(pos[l],pos[r],c);
+			}
+			
+//			cout<<"? "<<tmp<<endl;
+			mmin(ans,tmp);
+		}
+		if(ans!=inf) cout<<ans<<endl;
+		else cout<<-1<<endl;
+	}
+    return 0;
+}
+/*
+1
+6
+abcbaa
+*/
